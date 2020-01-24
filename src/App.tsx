1933755f42bf0c4782b0e85059d819a5c57816a0
Aleksey <gris.mego@gmail.com>
2020-01-24 18:39:42 +0700

    reset repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import data from './mock/movies.json';
-import { MoviesList } from './movies-list/movies-list';
 
 const App: React.FC = () => {
-    return <MoviesList {...data} />;
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 };
 
 export default App;
