4548a10c6ca291ff21fcd292c5f9f271c762a23c
Алексей Кунчий <gris.mego@gmail.com>
2020-02-02 14:21:03 +0700

    adding header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import 'normalize.css';
 import { MoviesList } from './movies-list/movies-list';
 import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import './index.css';
 
 const App: React.FC = () => {
     return (
         <>
             <Header />
             <MoviesList />
+            <Footer />
         </>
     );
 };
--- src/header/header.tsx
+++ src/header/header.tsx
@@ -4,14 +4,23 @@ import styles from './header.module.css';
 export const Header = () => {
     return (
         <header className={styles.header}>
-            <div>тут logo</div>
+            <div className={styles.logo}>MOViER+</div>
+            <div className={styles.search}>
+                <div>all</div>
+                <input type='text' placeholder='Search Movier+' />
+                <button>search</button>
+            </div>
             <div>
+                <img src='' alt='' />
+                <span>User Name</span>
+            </div>
+            {/* <div>
                 <ul className={styles.list}>
                     <li>фильтр1</li>
                     <li>фильтр2</li>
                     <li>фильтр3</li>
                 </ul>
-            </div>
+            </div> */}
         </header>
     );
 };
