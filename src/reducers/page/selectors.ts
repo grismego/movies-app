e7862c9a90762d5cf0fc50e1dd40a83e9aabe5db
Алексей Кунчий <gris.mego@gmail.com>
2020-02-09 20:02:17 +0700

    reselect

--- src/reducers/page/selectors.ts
+++ src/reducers/page/selectors.ts
@@ -8,6 +8,8 @@ export const addingFavoriteKey = createSelector(getMovies, movies =>
     movies.map((movie: MovieItem) => ({ ...movie, isFavorite: false }))
 );
 
+// const getMoviesByTitle = (movies, title) => movies.filter(movie => movie.title === title)
+
 export const getArraysGenre = createSelector(getMovies, getGenres);
 
 export const getUnicGenre = createSelector(getGenres, genres => new Set(genres.flat()));
