32e0383106cc781ae97f5ba4f00236aa9504f007
Алексей Кунчий <gris.mego@gmail.com>
2020-02-11 23:41:42 +0700

    adding genres

--- src/componetns/search/search.tsx
+++ src/componetns/search/search.tsx
@@ -4,7 +4,7 @@ import Autosuggest from 'react-autosuggest';
 
 import './theme.css';
 
-import { getMoviesTitle } from '../../reducers/selectors';
+import { getMoviesTitle, getUnicGenre } from '../../reducers/selectors';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { addSuggestion, addSearch } from '../../reducers/actions';
@@ -20,6 +20,8 @@ export const Search = () => {
     const sugRes = useSelector((state: RootStore) => state.suggestions);
 
     const moviesTitle = getMoviesTitle(movies);
+
+    const unicGenres = Array.from(getUnicGenre(movies));
 
     const escapeRegexCharacters = (str: string) => str.replace(/([ .\w']+?)(\W\d{4}\W?.*)/g, '\\$&');
 
@@ -37,37 +39,47 @@ export const Search = () => {
     };
 
     return (
-        <div className={styles.search}>
-            <Autosuggest
-                suggestions={suggestions}
-                onSuggestionsClearRequested={() => setSuggestions([])}
-                onSuggestionsFetchRequested={({ value }) => {
-                    setValue(value);
-                    setSuggestions(getSuggestions(value));
-                }}
-                alwaysRenderSuggestions={suggestions ? true : false}
-                getSuggestionValue={suggestion => suggestion}
-                renderSuggestion={suggestion => <span>{suggestion}</span>}
-                onSuggestionSelected={(_, { suggestionValue }) => {
-                    dispatch(addSuggestion(suggestionValue));
-                }}
-                inputProps={{
-                    placeholder: 'Search Movier+',
-                    value,
-                    onChange: (_, { newValue, method }) => {
-                        setValue(newValue);
-                    },
-                }}
-                highlightFirstSuggestion={true}
-            />
-            <button
-                onClick={() => {
-                    dispatch(addSearch(value));
-                    dispatch(addSuggestion(value));
-                }}
-            >
-                Search
-            </button>
+        <div className={styles.searchWrapper}>
+            <div className={styles.search}>
+                <select name='' id=''>
+                    <option value='All'>All</option>
+                    <option value='All'>Director</option>
+                    <option value='All'>Advanced</option>
+                </select>
+                <Autosuggest
+                    suggestions={suggestions}
+                    onSuggestionsClearRequested={() => setSuggestions([])}
+                    onSuggestionsFetchRequested={({ value }) => {
+                        setValue(value);
+                        setSuggestions(getSuggestions(value));
+                    }}
+                    alwaysRenderSuggestions={suggestions ? true : false}
+                    getSuggestionValue={suggestion => suggestion}
+                    renderSuggestion={suggestion => <span>{suggestion}</span>}
+                    onSuggestionSelected={(_, { suggestionValue }) => {
+                        dispatch(addSuggestion(suggestionValue));
+                    }}
+                    inputProps={{
+                        placeholder: 'Search Movier+',
+                        value,
+                        onChange: (_, { newValue, method }) => {
+                            setValue(newValue);
+                        },
+                    }}
+                    highlightFirstSuggestion={true}
+                />
+                <button
+                    onClick={() => {
+                        dispatch(addSearch(value));
+                        dispatch(addSuggestion(value));
+                    }}
+                >
+                    Search
+                </button>
+            </div>
+            {/* {unicGenres.map(genre => {
+                return <li>{genre}</li>;
+            })} */}
         </div>
     );
 };
